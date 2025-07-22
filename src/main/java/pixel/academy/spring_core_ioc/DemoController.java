257e24bdd2aa8ab239fb3150689f47b000bcad01
Sorin Gore <soringoredev@gmail.com>
2025-07-22 19:17:42 +0300

    Add GetMApping

--- src/main/java/pixel/academy/spring_core_ioc/DemoController.java
+++ src/main/java/pixel/academy/spring_core_ioc/DemoController.java
@@ -1,6 +1,7 @@
 package pixel.academy.spring_core_ioc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class DemoController {
     @Autowired
     public DemoController(Chef theChef) {
         myChef = theChef;
+    }
+
+    @GetMapping("/dailyRecipe")
+    public String getDailyRecipe() {
+        return myChef.getDailyRecipe();
     }
 }
